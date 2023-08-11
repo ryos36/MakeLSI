19b8f8744e5bb6ab44caf5adcddfdeed8f2c4ebc
Ryos Suzuki <ryos@sinby.com>
2023-08-11 22:43:02 +0900

    この inv_test_lvs_settings.rb の設定を入れておくと デバイスが同じことがわかるみたい。

---
+++ 2021/Inverter/KLayout/lvs_work/inv_test_lvs_settings.rb
@@ -0,0 +1,18 @@
+def lvs_settings
+  same_circuits 'int_test', '.TOP'
+  align
+  same_device_classes 'HRES', 'RES'
+  same_device_classes 'RES', 'RES'
+  same_device_classes 'NMOS', 'NCH'
+  same_device_classes 'PMOS', 'PCH'
+  tolerance 'HRES', 'R', :relative => 0.03
+  tolerance 'RES', 'R', :relative => 0.03
+  tolerance 'CAP', 'C', :relative => 0.03
+  netlist.make_top_level_pins
+  netlist.flatten_circuit 'Nch*'
+  netlist.flatten_circuit 'Pch*'
+  netlist.flatten_circuit 'R_poly*'
+  netlist.flatten_circuit 'HR_poly'
+  netlist.combine_devices
+  schematic.combine_devices
+end
