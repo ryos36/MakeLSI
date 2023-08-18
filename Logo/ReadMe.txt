4ceb1d7166a698cb2cee01cea95950cabff4b743
Ryos Suzuki <ryos@sinby.com>
2023-08-18 11:52:35 +0900

    Logo アップデート。Logo の ReadMe 追加

---
+++ Logo/ReadMe.txt
@@ -0,0 +1,9 @@
+nazca というツールを 
+pip install --user nazca-0.5.13
+として user にインストールしてつかった。
+pip では nazca って別にあるので注意。
+
+https://nazca-design.org/
+https://nazca-design.org/convert-image-to-gds/
+
+https://scrapbox.io/makelsi/Nazca%E3%81%A7%E7%94%BB%E5%83%8F%E3%82%92GDS%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%81%AB%E5%A4%89%E6%8F%9B%E3%80%90Python%E3%80%91
--- Logo/conv.py
+++ Logo/conv.py
@@ -1,4 +1,5 @@
+# use nazca-0.5.13
 import nazca as nd
 nd.image('sinby2048x2048-R.png', threshold=0.5, size=100, pixelsize=1.0, layer=10).put(0)
-nd.image('sinby2048x2048-L.jpg', threshold=0.5, size=100, pixelsize=1.0, layer=3,invert=True).put(2)
+nd.image('sinby2048x2048-L.png', threshold=0.5, size=100, pixelsize=1.0, layer=3,invert=True).put(2)
 nd.export_gds()
