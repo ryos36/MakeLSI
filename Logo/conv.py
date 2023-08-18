4ea2da51d1d0423ae9cfe13e6ff99a0be18ab682
Ryos Suzuki <ryos@sinby.com>
2023-08-18 10:52:26 +0900

    本物の VCO も動いた

--- Logo/conv.py
+++ Logo/conv.py
@@ -1,3 +1,4 @@
 import nazca as nd
-nd.image('Sinbino.png', threshold=0.5, size=100, pixelsize=1.0, layer=12).put(0)
+nd.image('sinby2048x2048-R.png', threshold=0.5, size=100, pixelsize=1.0, layer=10).put(0)
+nd.image('sinby2048x2048-L.jpg', threshold=0.5, size=100, pixelsize=1.0, layer=3,invert=True).put(2)
 nd.export_gds()
